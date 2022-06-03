5e64a7a3d17b966c2770eba5beb3cfc8172aaf6d
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-06-03 20:32:28 +0000

    submit entry function

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-// var data = {
-//   daysOfTheWeek: []
-// }
-var $addEntry = document.querySelector('#add-entry-btn');
-function addEntry() {
-  $addEntry.className = 'modal';
-}
+var data = {
+  sun: [],
+  mon: [],
+  tues: [],
+  wed: [],
+  thurs: [],
+  fri: [],
+  sat: []
+};
 
+var $addEntry = document.querySelector('#add-entry-btn');
 var $submit = document.querySelector('#submit-btn');
-function submitFire() {
-  $submit.className = 'modal-hidden';
-}
+var $modal = document.querySelector('.modal');
+var $dayOfWeek = document.querySelector('.day-of-week');
+var $time = document.querySelector('.time');
+var $description = document.querySelector('.text-area');
 
 $addEntry.addEventListener('click', addEntry);
 $submit.addEventListener('click', submitFire);
+
+function addEntry() {
+  $modal.className = 'modal';
+}
+
+function submitFire() {
+  $modal.className = 'modal hidden';
+  var entry = {
+    dayOfWeek: $dayOfWeek.value,
+    time: $time.value,
+    description: $description.value
+  };
+  data[entry.dayOfWeek].push(entry);
+}
