6c09320c2c42b9a2c570f39938c67515646ed8a2
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-06-03 20:14:14 +0000

    modal event listeners

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 // var data = {
 //   daysOfTheWeek: []
 // }
+var $addEntry = document.querySelector('#add-entry-btn');
+function addEntry() {
+  $addEntry.className = 'modal';
+}
+
+var $submit = document.querySelector('#submit-btn');
+function submitFire() {
+  $submit.className = 'modal-hidden';
+}
+
+$addEntry.addEventListener('click', addEntry);
+$submit.addEventListener('click', submitFire);
