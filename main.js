f3519f37928dc72b45c59a74cd4575487325a2ae
Yuhan Zhang <zyh514312@gmail.com>
2022-06-03 22:38:04 +0000

    button change context

--- main.js
+++ main.js
@@ -14,11 +14,19 @@ var $modal = document.querySelector('.modal');
 var $dayOfWeek = document.querySelector('.day-of-week');
 var $time = document.querySelector('.time');
 var $description = document.querySelector('.text-area');
-var $dayButton = document.querySelector('.day');
+var $dayButton = document.querySelector('.weekday');
 
+var $scheduledEvent = document.querySelector('#scheduledEvent');
 $addEntry.addEventListener('click', addEntry);
 $submit.addEventListener('click', submitFire);
-$dayButton.addEventListener('click', function () {});
+$dayButton.addEventListener('click', function (event) {
+
+  if (event.target.tagName === 'BUTTON') {
+    $scheduledEvent.textContent = 'Scheduled Events for ' + event.target.getAttribute('data-day');
+
+  }
+
+});
 
 function addEntry() {
   $modal.className = 'modal';
